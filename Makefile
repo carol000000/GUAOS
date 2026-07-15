bafa8536476fe8c5a1493fe686a6485e9776d091
carol000000 <carol2008tw@gmail.com>
2026-07-15 23:29:10 +0800

    run

--- Makefile
+++ Makefile
@@ -1,11 +1,23 @@
-cc = gcc
+CC = gcc
+LD = ld
+
 CFLAGS = -ffreestanding -m64 -Wall -Wextra
 
-all: build/kernel.o
+ISO = build/GUAOS.iso
+
+all: $(ISO)
 
 build/kernel.o: kernel/kernel.c
 	mkdir -p build
-	$(cc) $(CFLAGS) -c kernel/kernel.c -o build/kernel.o
+	$(CC) $(CFLAGS) -c kernel/kernel.c -o build/kernel.o
+
+build/kernel.elf: build/kernel.o linker.ld
+	$(LD) -T linker.ld -o build/kernel.elf build/kernel.o
+
+$(ISO): build/kernel.elf
+	mkdir -p iso/boot
+	cp build/kernel.elf iso/boot/kernel.elf
+	grub-mkrescue -o $(ISO) iso
 
 clean:
 	rm -rf build
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,29 @@
 void kernel_main(void){
-    
+    char *video = (char *)0xB8000;
+    video[0] = 'H';
+    video[1] = 0x02;
+    video[2] = 'E';
+    video[3] = 0x02;
+    video[4] = 'L';
+    video[5] = 0x02;
+    video[6] = 'L';
+    video[7] = 0x02;
+    video[8] = 'O';
+    video[9] = 0x02;
+
+    video[12] = 'G';
+    video[13] = 0x02;
+    video[14] = 'U';
+    video[15] = 0x02;
+    video[16] = 'A';
+    video[17] = 0x02;
+    video[18] = 'O';
+    video[19] = 0x02;
+    video[20] = 'S';
+    video[21] = 0x02;
+    video[22] = '!';
+    video[23] = 0x02;
+
     while(1){
         
     }
